1fbad0cf6114c411476fd15694f919aa171109ae
MaoScorpio21 <3917@holbertonschool.com>
2021-10-10 18:13:10 -0500

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
+#include<stdio.h>
+
 /**
- * main - Entry point
- *
+ * main - Write a C program that prints exactly: with proper grammar, but the
+ *        outcome is a piece of art,, followed by a new line
  * Return: Always 0 (Success)
  */
-#include<stdio.h>
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
